69d1ca64ff7540d4715fdac271b3f13353554b1d
周博 <bo.zhou@longleding.com>
2021-09-28 19:10:01 +0800

    update to v0.1.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/riiy/spwm',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     entry_points={
         "saleor.plugins": [
--- spwm/plugin.py
+++ spwm/plugin.py
@@ -1,11 +1,19 @@
+import os
 import json
 import logging
+import requests
 from typing import TYPE_CHECKING, Any, List, Optional, Union
 
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.core.handlers.wsgi import WSGIRequest
+from django.contrib.auth import get_user_model
+from django.core.signing import Signer
 
 from saleor.plugins.base_plugin import BasePlugin
+from saleor.core.auth import get_token_from_request
+
+User = get_user_model()
+signer = Signer()
 
 
 class SpwmPlugin(BasePlugin):
@@ -19,6 +27,32 @@ class SpwmPlugin(BasePlugin):
         # check signatures and headers.
         if path == '/get-token/':
             # do something with the request
-            return JsonResponse(data={"token":True})
+            data = request.data
+            open_id = OpenidUtils(data["code"]).get_openid()
+            user, _ = User.objects.get_or_create(email=f"wx_{open_id}")
+            token = signer.sign(user.id)
+            return JsonResponse(data={"token": token})
 
         return HttpResponseNotFound()
+
+    def authenticate_user(self, request: WSGIRequest, previous_value) -> Optional["User"]:
+        """."""
+
+        token = get_token_from_request(request)
+        uid = signer.unsign(token)
+        user = User.objects.get(uid)
+        return user
+
+
+class OpenidUtils(object):
+    def __init__(self, jscode):
+        self.url = "https://api.weixin.qq.com/sns/jscode2session"
+        self.appid = os.environ.get('WX_MP_APPID')
+        self.secret = os.environ.get'WX_MP_APPSECRET')
+        self.jscode = jscode
+
+    def get_openid(self):
+        url = (self.url + "?appid=" + self.appid + "&secret=" + self.secret + "&js_code=" + self.jscode + "&grant_type=authorization_code")
+        r = requests.get(url)
+        openid = r.json()["openid"]
+        return openid
